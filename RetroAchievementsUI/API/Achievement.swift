6babcdd7f66ac6091de992ad3d697e7a1091a1f4
mrosenbergtech <108553702+mrosenbergtech@users.noreply.github.com>
2024-06-14 11:41:02 -0600

    Add: Achievement Sorting, Move Achievement Struct to New File

---
+++ RetroAchievementsUI/API/Achievement.swift
@@ -0,0 +1,46 @@
+//
+//  Achievement.swift
+//  RetroAchievementsUI
+//
+//  Created by Michael Rosenberg on 6/13/24.
+//
+
+import Foundation
+
+struct Achievement: Codable {
+    let id: Int
+    let numAwarded: Int
+    let numAwardedHardcore: Int
+    let title: String
+    let description: String
+    let points: Int
+    let trueRatio: Int
+    let author: String
+    let dateModified: String
+    let dateCreated: String
+    let badgeName: String
+    let displayOrder: Int
+    let memAddr: String
+    let type: String?
+    let dateEarnedHardcore: String?
+    let dateEarned: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "ID"
+        case numAwarded = "NumAwarded"
+        case numAwardedHardcore = "NumAwardedHardcore"
+        case title = "Title"
+        case description = "Description"
+        case points = "Points"
+        case trueRatio = "TrueRatio"
+        case author = "Author"
+        case dateModified = "DateModified"
+        case dateCreated = "DateCreated"
+        case badgeName = "BadgeName"
+        case displayOrder = "DisplayOrder"
+        case memAddr = "MemAddr"
+        case type = "type"
+        case dateEarnedHardcore = "DateEarnedHardcore"
+        case dateEarned = "DateEarned"
+    }
+}
--- RetroAchievementsUI/Views/Detail/AchievementsView.swift
+++ RetroAchievementsUI/Views/Detail/AchievementsView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct AchievementsView: View {
     @Binding var hardcoreMode: Bool
+    @State var selectedAchievementFilter: String = "All"
     var gameSummary: GameSummary
+    
     
     var body: some View {
         Form {
             Section(header: Text("Achievements")){
-                ForEach(gameSummary.achievements.keys.sorted(), id: \.self) { id in
-                    AchievementDetailView(hardcoreMode: $hardcoreMode, achievement: gameSummary.achievements[id]!)
+                HStack{
+                    Spacer()
+                    
+                    Text("All")
+                        .onTapGesture {
+                            selectedAchievementFilter = "All"
+                        }
+                        .fontWeight((selectedAchievementFilter == "All") ? .bold : .regular)
+                        .padding()
+                        .background(selectedAchievementFilter == "All" ? .gray : .clear)
+                        .clipShape(.rect(cornerRadius: 10))
+                    
+                    
+                    Spacer()
+                    
+                    Text("Locked")
+                        .onTapGesture {
+                            selectedAchievementFilter = "Locked"
+                        }
+                        .fontWeight((selectedAchievementFilter == "Locked") ? .bold : .regular)
+                        .padding()
+                        .background(selectedAchievementFilter == "Locked" ? .gray : .clear)
+                        .clipShape(.rect(cornerRadius: 10))
+                    
+                    Spacer()
+                    
+                    Text("Unlocked")
+                        .onTapGesture {
+                            selectedAchievementFilter = "Unlocked"
+                        }
+                        .fontWeight((selectedAchievementFilter == "Unlocked") ? .bold : .regular)
+                        .padding()
+                        .background(selectedAchievementFilter == "Unlocked" ? .gray : .clear)
+                        .clipShape(.rect(cornerRadius: 10))
+                    
+                    Spacer()
+                }
+                
+                if selectedAchievementFilter == "All" {
+                    ForEach(gameSummary.achievements.keys.sorted(), id: \.self) { id in
+                        AchievementDetailView(hardcoreMode: $hardcoreMode, achievement: gameSummary.achievements[id]!)
+                    }
+                } else if selectedAchievementFilter == "Locked" {
+                    ForEach(gameSummary.achievements.keys.filter { gameSummary.achievements[$0]!.dateEarnedHardcore == nil }.sorted(), id: \.self) { id in
+                        AchievementDetailView(hardcoreMode: $hardcoreMode, achievement: gameSummary.achievements[id]!)
+                    }
+                } else if selectedAchievementFilter == "Unlocked" {
+                    ForEach(gameSummary.achievements.keys.filter { gameSummary.achievements[$0]!.dateEarnedHardcore != nil }.sorted(), id: \.self) { id in
+                        AchievementDetailView(hardcoreMode: $hardcoreMode, achievement: gameSummary.achievements[id]!)
+                    }
                 }
             }
             
@@ -29,7 +79,7 @@ struct AchievementsView: View {
     @State var hardcoreMode: Bool = true
     let previewAchievementsSetJSONString: String =
 """
-{"48638":{"ID":48638,"NumAwarded":26359,"NumAwardedHardcore":14360,"Title":"A New Journey","Description":"Grab your first Power Star.","Points":1,"TrueRatio":1,"Author":"SamuraiGoroh","DateModified":"2023-06-18 14:47:41","DateCreated":"2017-05-25 17:37:31","BadgeName":"84220","DisplayOrder":1,"MemAddr":"96fd0806cdff4b8faa947aacfedcf86c","type":"progression","DateEarnedHardcore":"2023-01-14 21:24:32","DateEarned":"2023-01-14 21:24:32"},"48639":{"ID":48639,"NumAwarded":14002,"NumAwardedHardcore":9093,"Title":"Ready to Fight Bowser","Description":"Grab 8 Power Stars.","Points":3,"TrueRatio":3,"Author":"SamuraiGoroh","DateModified":"2023-06-18 14:47:49","DateCreated":"2017-05-25 17:37:37","BadgeName":"84221","DisplayOrder":2,"MemAddr":"b79fbd24b3ef74ed70e4dc7bf9afecbd","type":"progression","DateEarnedHardcore":"2023-01-14 22:07:45","DateEarned":"2023-01-14 22:07:45"},"48640":{"ID":48640,"NumAwarded":7304,"NumAwardedHardcore":5308,"Title":"Ready to Rematch Bowser","Description":"Grab 30 Power Stars.","Points":5,"TrueRatio":8,"Author":"SamuraiGoroh","DateModified":"2023-06-18 14:47:57","DateCreated":"2017-05-25 17:37:43","BadgeName":"84222","DisplayOrder":3,"MemAddr":"d2cd200a84ff9fb36935d7f3710b081d","type":"progression","DateEarnedHardcore":"2023-01-15 23:58:08","DateEarned":"2023-01-15 23:58:08"}
+{"48638":{"ID":48638,"NumAwarded":26359,"NumAwardedHardcore":14360,"Title":"A New Journey","Description":"Grab your first Power Star.","Points":1,"TrueRatio":1,"Author":"SamuraiGoroh","DateModified":"2023-06-18 14:47:41","DateCreated":"2017-05-25 17:37:31","BadgeName":"84220","DisplayOrder":1,"MemAddr":"96fd0806cdff4b8faa947aacfedcf86c","type":"progression","DateEarnedHardcore":"2023-01-14 21:24:32","DateEarned":"2023-01-14 21:24:32"},"48639":{"ID":48639,"NumAwarded":14002,"NumAwardedHardcore":9093,"Title":"Ready to Fight Bowser","Description":"Grab 8 Power Stars.","Points":3,"TrueRatio":3,"Author":"SamuraiGoroh","DateModified":"2023-06-18 14:47:49","DateCreated":"2017-05-25 17:37:37","BadgeName":"84221","DisplayOrder":2,"MemAddr":"b79fbd24b3ef74ed70e4dc7bf9afecbd","type":"progression","DateEarnedHardcore":"2023-01-14 22:07:45","DateEarned":"2023-01-14 22:07:45"},"48640":{"ID":48640,"NumAwarded":7304,"NumAwardedHardcore":5308,"Title":"Ready to Rematch Bowser","Description":"Grab 30 Power Stars.","Points":5,"TrueRatio":8,"Author":"SamuraiGoroh","DateModified":"2023-06-18 14:47:57","DateCreated":"2017-05-25 17:37:43","BadgeName":"84222","DisplayOrder":3,"MemAddr":"d2cd200a84ff9fb36935d7f3710b081d","type":"progression"}
 }
 """
     
