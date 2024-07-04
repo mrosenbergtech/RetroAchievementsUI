c3ed799f2b77ebb38f16d99b060fd71e49585878
mrosenbergtech <108553702+mrosenbergtech@users.noreply.github.com>
2024-07-04 00:48:00 -0600

    Fix: Improve Login Flow (Again Again)

--- RetroAchievementsUI/Views/Sub/RetroAchievementsLoginView.swift
+++ RetroAchievementsUI/Views/Sub/RetroAchievementsLoginView.swift
@@ -13,57 +13,70 @@ struct RetroAchievementsLoginView: View {
     @Binding var webAPIUsername: String
     @Binding var webAPIKey: String
     @State var blankCredentials: Bool = false
+    @State var loginPending: Bool = false
     
     var body: some View {
         Form{
             Section(
                 header: Text("RetroAchievements Login"), content: {
-                    HStack {
-                        Spacer()
-                        Text("Authenticated:")
-                        Image(systemName: "x.circle")
-                            .foregroundColor(.red)
-                        Spacer()
-                    }                    
-                    .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
-                    
-                    TextField("Username", text: $webAPIUsername)
-                        .multilineTextAlignment(.center)
-                        .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
-
-      
-                    SecureField("Web API Key", text: $webAPIKey)
-                        .multilineTextAlignment(.center)
-                        .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
-
-                    
-                    Button {
-                        Task {
-                            await network.authenticateCredentials(webAPIUsername: webAPIUsername, webAPIKey: webAPIKey)
-                        }
-                    } label: {
-                        HStack{
-                            Spacer()
-                            Text("Login")
-                            Spacer()
-                        }        
-                        .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
-                    }
-                    
-                    Button {
-                        guard let url = URL(string: "https://api-docs.retroachievements.org/getting-started.html#get-your-web-api-key") else {
-                             return
-                        }
-
-                        if UIApplication.shared.canOpenURL(url) {
-                            print("Opening RA Login Credentials URL")
-                             UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                        }
-                    } label: {
+                    if loginPending {
                         HStack {
                             Spacer()
-                            Text("Get Login Credentials")
+                            ProgressView()
                             Spacer()
+                        }
+                    } else {
+                        
+                        HStack {
+                            Spacer()
+                            Text("Authenticated:")
+                            Image(systemName: "x.circle")
+                                .foregroundColor(.red)
+                            Spacer()
+                        }
+                        .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
+                        
+                        TextField("Username", text: $webAPIUsername)
+                            .multilineTextAlignment(.center)
+                            .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
+                        
+                        
+                        SecureField("Web API Key", text: $webAPIKey)
+                            .multilineTextAlignment(.center)
+                            .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
+                        
+                        
+                        Button {
+                            loginPending = true
+                            Task {
+                                await network.authenticateCredentials(webAPIUsername: webAPIUsername, webAPIKey: webAPIKey)
+                                
+                                if !network.webAPIAuthenticated { loginPending = false }
+                            }
+                        } label: {
+                            HStack{
+                                Spacer()
+                                Text("Login")
+                                Spacer()
+                            }
+                            .alignmentGuide(.listRowSeparatorLeading) { _ in -20 }
+                        }
+                        
+                        Button {
+                            guard let url = URL(string: "https://api-docs.retroachievements.org/getting-started.html#get-your-web-api-key") else {
+                                return
+                            }
+                            
+                            if UIApplication.shared.canOpenURL(url) {
+                                print("Opening RA Login Credentials URL")
+                                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                            }
+                        } label: {
+                            HStack {
+                                Spacer()
+                                Text("Get Login Credentials")
+                                Spacer()
+                            }
                         }
                     }
                 }
